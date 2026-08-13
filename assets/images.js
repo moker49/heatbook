6846afc793fa269aa026e0e1ffb0c60ddc0c07de
Efren <moker49@gmail.com>
2026-08-13 09:23:10 -0400

    generic

---
+++ assets/images.js
@@ -0,0 +1,34 @@
+window.HEATBOOK_IMAGES = {
+  food: [
+    'icons8-asparagus-96.png',
+    'icons8-beef-96.png',
+    'icons8-bread-96.png',
+    'icons8-broccoli-96.png',
+    'icons8-broccolini-96.png',
+    'icons8-carrot-96.png',
+    'icons8-cucumber-96.png',
+    'icons8-eggplant-96.png',
+    'icons8-eggs-96.png',
+    'icons8-french-fries-96.png',
+    'icons8-mushroom-96.png',
+    'icons8-pizza-96.png',
+    'icons8-plaintains-96.png',
+    'icons8-potato-96.png',
+    'icons8-poultry-leg-96.png',
+    'icons8-rice-bowl-96.png',
+    'icons8-sweet-potato-96.png',
+    'icons8-taco-96.png',
+    'icons8-whole-fish-96.png',
+    'icons8-zucchini-96.png'
+  ],
+  generic: [
+    'icons8-cookies-96.png',
+    'icons8-fruits-96.png',
+    'icons8-leaf-96.png',
+    'icons8-paella-96.png',
+    'icons8-popcorn-96.png',
+    'icons8-salad-96.png',
+    'icons8-toast-96.png',
+    'icons8-wheat-96.png'
+  ]
+};
